813ed771cdd701cac36c566d038bda2703cc2d8d
ifazzzz <shoaibahmedifaz@gmail.com>
2022-09-03 11:05:52 +0600

    categories added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+const loadCategories = () =>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+    .then((response) => response.json())
+    .then(data => displayCategories(data.data.news_category)) 
+}
+
+const displayCategories = (categories) =>{
+    //  categories.forEach(category =>{
+    //     console.log(category);
+    //  })
+    const categoryContainer = document.getElementById('category-container');
+    categories.forEach(category =>{
+        console.log(category);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <button class="btn btn-link">${category.category_name}</button>
+        `;
+        categoryContainer.appendChild(div);
+
+    })
+    
+}
+loadCategories();
+
